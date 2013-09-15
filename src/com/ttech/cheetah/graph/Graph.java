61040c7aba8ed41d8c9b84bfd99c108e0395be2e
burakkk <burak.isikli@gmail.com>
2013-09-15 23:15:36 +0300

    Initial commit

---
+++ src/com/ttech/cheetah/graph/Graph.java
@@ -0,0 +1,8 @@
+package com.ttech.cheetah.graph;
+
+public class Graph {
+	public Graph(int edgeCount){
+		this.vertexList = new int[edgeCount][2];
+	}
+	private int [][] vertexList;
+}
---
+++ src/com/ttech/cheetah/test/Main.java
@@ -0,0 +1,11 @@
+package com.ttech.cheetah.test;
+
+public class Main {
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) throws Exception{
+		
+	}
+}
