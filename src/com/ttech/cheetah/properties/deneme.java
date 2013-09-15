e775a6128bf3a81df12a7174548aaceab44be755
burakkk <burak.isikli@gmail.com>
2013-09-15 23:18:54 +0300

    Initial

---
+++ src/com/ttech/cheetah/properties/deneme.java
@@ -0,0 +1,13 @@
+package com.ttech.cheetah.properties;
+
+public class deneme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
